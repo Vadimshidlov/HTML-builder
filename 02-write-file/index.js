98c58b7078689901542addf1272d2bd10a601149
Vadimshidlov <mat23vadim@mail.ru>
2023-05-07 00:01:09 +0300

    fix: fixed eslint warnings for task 02-wrtite-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,38 +1,38 @@
-const path = require("node:path");
-const fs = require("fs");
-const process = require("node:process");
+const path = require('node:path');
+const fs = require('fs');
+const process = require('node:process');
 const pathToFile = path.join(__dirname);
-const readline = require("node:readline").createInterface({
+const readline = require('node:readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-console.log("Hello, my dear friend");
+console.log('Hello, my dear friend');
 
 function createFile() {
-  fs.open(`${pathToFile}/newFile.txt`, "w", (err) => {
+  fs.open(`${pathToFile}/newFile.txt`, 'w', (err) => {
     if (err) throw err;
     // console.log("file created");
   });
 }
 createFile();
 
-process.on("SIGINT", (signal) => {
+process.on('SIGINT', (signal) => {
   process.exit();
 });
 
-process.on("exit", () => {
+process.on('exit', () => {
   readline.close();
-  console.log("Goodbye, my dear friend!");
+  console.log('Goodbye, my dear friend!');
 });
 
 function getQuestion() {
-  readline.question("", (answer) => {
-    if (answer === "exit") {
+  readline.question('', (answer) => {
+    if (answer === 'exit') {
       process.exit();
     }
 
-    fs.appendFile(`${pathToFile}/newFile.txt`, answer + "\n", (err) => {
+    fs.appendFile(`${pathToFile}/newFile.txt`, answer + '\n', (err) => {
       if (err) {
         console.error(err);
       }
