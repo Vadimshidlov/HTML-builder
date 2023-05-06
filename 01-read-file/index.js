7c9dc7d00b5e85e3b76820e7004c1bfdab9c4dc6
Vadimshidlov <mat23vadim@mail.ru>
2023-05-06 23:58:07 +0300

    fix: fixed solution taks 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,8 @@
-const path = require("node:path");
-const fs = require("fs");
+const path = require('node:path');
+const fs = require('fs');
+const pathToFile = path.join(__dirname, 'text.txt');
 
-const pathToFile = path.join(__dirname, "text.txt");
-fs.readFile(pathToFile, "utf8", function (error, data) {
-  if (error) throw error;
-  console.log(data);
+const readStream = fs.createReadStream(pathToFile, { encoding: 'utf-8' });
+readStream.on('data', (text) => {
+  console.log(text);
 });
